9cac2035dc5d5dd1b2addf7c806594205d9b1bc5
Sebastian Ruiz <sruizramirez11@gmail.com>
2023-06-10 17:09:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Resultado from './Resultado'
 
-
 const App = () => {
-
   const [text, setText] = useState("");
   const [meanings, setMeanings] = useState([]);
   const [phonetics, setPhonetics] = useState([]);
@@ -23,36 +21,59 @@ const App = () => {
     })
     .catch(err => setError(err))
   }
-  
   useEffect(() => {
-  if(!text.trim()) return;
-  const debounce = setTimeout(() => {
-    dictionaryApi(text)
-  }, 800);
-  return () => clearTimeout(debounce)
+ 
+    function toggleDark(){
+      const body = document.querySelector("body");
+      const toggleBtn = document.getElementById("toggle");
+  
+      toggleBtn.addEventListener("click", function(){
+        if (toggleBtn.checked) {
+          body.classList.add("black")
+          body.classList.remove("light")
+        } else {
+          body.classList.add("light")
+          body.classList.remove("black")
+        }
+      })
+      const textarea = document.querySelector('textarea');
+
+      textarea.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  });
+    }
+    toggleDark();
+    if(!text.trim()) return;
+    const debounce = setTimeout(() => {
+      dictionaryApi(text)
+    }, 500);
+    return () => clearTimeout(debounce)
   }, [text])
   
+    
   return (
-  <div className="container">
+  <div className="container" id='container'>
     <h1>English Dictionary</h1>
     <header>
       <div className='book'>
       <i className="fa-solid fa-book"></i>
       </div>
-      {/* <div className='moon'>
+      <div className='moon'>
         <div className="cl-toggle-switch">
           <label className="cl-switch">
-            <input type="checkbox" />
+            <input type="checkbox" id='toggle' />
             <span></span>
           </label>
         </div>
         <i className="fa-solid fa-moon"></i>
-      </div> */}
+      </div>
     </header>
       <form>
         <div className='row'>
-          <textarea cols="" rows="" value={text} 
-          onChange={e => setText(e.target.value)} />
+        <textarea cols="1" rows="1" value={text} 
+        onChange={e => setText(e.target.value)} id='textarea' />
           <div className='search_icon'>
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
